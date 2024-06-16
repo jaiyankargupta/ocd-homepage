a6c7ce4a36ec319002f8245699b14336d836769c
Himanshu-AT <aristech.official@gmail.com>
2024-06-16 19:43:10 +0530

    remove build error

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -5,17 +5,18 @@ import { useEffect, useState } from "react";
 const loginPage = () => {
   const [emailSumbitted, setEmailSumbitted] = useState(false);
 
-  useEffect(() => {
-    if (emailSumbitted) {
-      window.location.href = "/home";
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (emailSumbitted) {
+  //     window.location.href = "/home";
+  //   }
+  // }, []);
+
   return (
     <main className=" mb-14 h-full w-full flex justify-center items-center">
       <div className="mt-32 py-8 px-6 backdrop:blur-lg flex w-2/3 flex-col border rounded-lg bg-gradient-to-b from-transparent via-purple-900 to-transparent bg-opacity-30 shadow-inner">
         <div className="text-white text-4xl mb-2">Authenticate Yourself</div>
         <div className="text-white text-2xl mb-6">
-          Provide your college ID and we'll Authenticate you
+          Provide your college ID and we&apos;ll Authenticate you
         </div>
         <form className="flex flex-col gap-4 z-20">
           <input
