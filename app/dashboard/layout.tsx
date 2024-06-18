e28b6aaeb84603d652aa436c3362543f203a9db3
Himanshu-AT <aristech.official@gmail.com>
2024-06-18 09:46:56 +0530

    setup demo dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,6 +4,7 @@ import "./../globals.css";
 import Navbar from "@/components/main/Navbar";
 import Footer from "@/components/main/Footer";
 import Dashboard from "./page";
+import StarsCanvas from "@/components/main/StarBackground";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +23,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
-        {/* <StarsCanvas /> */}
+        <StarsCanvas />
         {/* <Navbar /> */}
         <Dashboard />
         {/* <Footer /> */}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,14 @@
 export default function Dashboard() {
   return (
     <main className="h-full w-full text-white">
-      <div className="flex flex-col gap-20">Just a dashboard</div>
+      <div className="flex flex-col gap-20">
+        <div>Name</div>
+        <div>email</div>
+        <div>Other details like github stats, cp stats, proj, exp so on...</div>
+        {/*
+         * // TODO connect with @OpenEdu's prublic profile builder
+         */}
+      </div>
     </main>
   );
 }
